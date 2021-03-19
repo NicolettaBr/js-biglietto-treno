de9cbd13ebec937ce72cfd9735f50053f43cedc8
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-03-19 15:24:03 +0100

    creazione file js-inizio esercizio

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Chiedere all' utente il numero di km che vuole percorrere (numero)
+var numeroKm = prompt( ' Quanti km devi percorrere? ' );
+console.log(numeroKm);
+var numeroKmAsNumber = parseInt(numeroKm);
+console.log(numeroKmAsNumber);
+
+// Chiedere l' età del passeggero (stringa)
+
+
+// Calcolo prezzo del biglietto in base ai km (0.21 € al km) (numero)
+
+// Calcolo prezzo biglietto con sconto 20% per minorenni e 40% over 65 (numero)
